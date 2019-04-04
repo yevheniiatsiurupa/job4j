eba2cc5798c1e6f74f89ebae7a9d728a887803cb
yevheniiatsiurupa <tsiurupa_yev@mail.ru>
2019-04-04 21:13:52 +0300

    1.4. JUnit[#127498]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Evgeniya Tsiurupa
+ * @version 1.0
+ * @since 04/04/2019
+ */
+public class CalculateTest {
+ /**
+ * Test echo.
+ */
+
+@Test
+ public void whenTakeNameThenThreeEchoPlusName() {
+    String input = "Evgeniya";
+    String expect = "Echo, echo, echo : Evgeniya"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+ }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for testing.
+*
+* @author Evgeniya Tsiurupa
+* @version 1.0
+* @since 04/04/2019
+*/
+package ru.job4j.calculate;
