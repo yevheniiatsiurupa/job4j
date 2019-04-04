e4de8c48e68715b838cefb8066b09adfb1f520f5
yevheniiatsiurupa <tsiurupa_yev@mail.ru>
2019-04-04 20:04:25 +0300

    1.3. JavaDoc. Code convention[#127497]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,15 @@
 package ru.job4j.calculate;
+
+/**
+  * Calculate.
+  * Выводит на консоль Hello World!!
+  * @author Evgeniya Tsiurupa 
+  */
 public class Calculate {
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main (String [] args){
 	System.out.println ("Hello World!!");
 	}
