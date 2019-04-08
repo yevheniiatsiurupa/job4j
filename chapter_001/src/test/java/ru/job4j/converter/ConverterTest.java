4fe17b88ab8837b91757bddf8525ef7569709e8a
yevheniiatsiurupa <tsiurupa_yev@mail.ru>
2019-04-08 18:51:28 +0300

    3.1. Элементарный калькулятор.[#127487] c javadoc

---
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Evgeniya Tsiurupa
+ * @version 1.0
+ * @since 08/04/2019
+ */
+
+public class ConverterTest {
+    /**
+     * Test rubleToDollar.
+     */
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Test rubleToEuro.
+     */
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(70);
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Test euroToRuble.
+     */
+    @Test
+    public void when1EuroToRubleThen70() {
+        Converter converter = new Converter();
+        int result = converter.euroToRuble(1);
+        assertThat(result, is(70));
+    }
+    /**
+     * Test dollarToRuble.
+     */
+    @Test
+    public void when1DollarToRubleThen60() {
+        Converter converter = new Converter();
+        int result = converter.dollarToRuble(1);
+        assertThat(result, is(60));
+    }
+}
