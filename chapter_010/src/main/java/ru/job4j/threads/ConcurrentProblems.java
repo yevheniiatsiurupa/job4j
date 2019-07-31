87ebf2f275cff0e48cd8a58d9196968d7cea02f4
yevheniiatsiurupa <tsiurupa_yev@mail.ru>
2019-07-31 09:45:42 +0300

    1. Проиллюстрировать проблемы с многопоточностью.[#164690]

---
+++ chapter_010/src/main/java/ru/job4j/threads/ConcurrentProblems.java
@@ -0,0 +1,41 @@
+package ru.job4j.threads;
+
+public class ConcurrentProblems {
+    private static int shared = 0;
+
+    public static void main(String[] args) {
+        new Thread(new ChangeOne()).start();
+        new Thread(new ChangeTwo()).start();
+    }
+
+    static class ChangeOne implements Runnable {
+
+        @Override
+        public void run() {
+            int localValue = shared;
+            while (localValue < 5) {
+                if (localValue != shared) {
+                    System.out.println("Value was changed:" + shared);
+                    localValue = shared;
+                }
+            }
+        }
+    }
+
+    static class ChangeTwo implements Runnable {
+
+        @Override
+        public void run() {
+            int localValue = shared;
+            while (shared < 5) {
+                shared = ++localValue;
+                System.out.println("Incrementing shared number: " + shared);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ chapter_010/src/main/java/ru/job4j/threads/DepositExample.java
@@ -0,0 +1,30 @@
+package ru.job4j.threads;
+
+public class DepositExample implements Runnable {
+    private static boolean flag = false;
+    public void changeFlag() {
+        flag = true;
+        System.out.println("Flag was changed");
+    }
+
+    @Override
+    public void run() {
+        while (!flag) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("Before change");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("After change.");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        DepositExample de = new DepositExample();
+        Thread t = new Thread(de);
+        t.start();
+        Thread.sleep(5000);
+        de.changeFlag();
+    }
+}
