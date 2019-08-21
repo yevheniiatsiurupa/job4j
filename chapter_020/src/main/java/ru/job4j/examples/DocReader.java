45be13505888db6614341b79bea865b00d2af0bd
yevheniiatsiurupa <tsiurupa_yev@mail.ru>
2019-08-21 09:43:52 +0300

    chapter 020 правка

--- chapter_020/src/main/java/ru/job4j/examples/DocReader.java
+++ chapter_020/src/main/java/ru/job4j/examples/DocReader.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class DocReader {
     public static void main(String[] args) throws Exception {
-        System.setProperty("console.encoding","utf-8");
+        System.setProperty("console.encoding", "utf-8");
         File file = new File("chapter_020/src/main/resources/text.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
 //        PrintWriter pw = new PrintWriter(new FileOutputStream(new File("chapter_020/src/main/resources/read.txt")));
